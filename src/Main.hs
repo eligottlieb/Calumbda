945caab19d54dfa13417709bfa1fc276babab08a
Eli Sennesh <eligottlieb@gmail.com>
2015-08-21 12:26:27 -0400

    exponential: fresh branch for the requirement that Calude halting times be exponentially large
    Unsure of whether Calude's Anytime Algorithm genuinely requires the associated
    halting time to be super-exponential.  Branching to handle this possibility.
    
    Tests done:
    Signed-off by: Eli Sennesh <elisennesh@gmail.com>

--- src/Main.hs
+++ src/Main.hs
@@ -9,26 +9,30 @@ import PartialT
 import UntypedLambda
 import Parser
 
-confidenceSteps :: Double -> Integer
-confidenceSteps q | q >= 0 && q < 1 = ceiling $ (1 / (1-q)) ** 2
-stepsConfidence :: Integer -> Double
-stepsConfidence 0 = 0.0
-stepsConfidence 1 = 0.0
-stepsConfidence m = 1 - 1 / sqrt (fromIntegral m)
+confidenceSteps :: ProgramSize a => Double -> a -> Integer
+confidenceSteps q a| q >= 0 && q < 1 = max m m' where
+  m = 2^(2 * size a) + 1
+  m' = ceiling $ (1 / (1-q)) ** 2
+stepsConfidence :: ProgramSize a => Integer -> a -> Double
+stepsConfidence m a =
+  if m >= 2^(2 * size a) + 1 then
+    1 - 1 / sqrt (fromIntegral m)
+  else
+    0.0
 
 data Calude = Normalized Value | Failed EvalError |
-              NotYet Integer (ThrowsPartial StuckState Value)
+              NotYet Integer Term (ThrowsPartial StuckState Value)
 instance Show Calude where
   show (Normalized v) = "Normalized: " ++ show v
   show (Failed e) = "Evaluation error: " ++ show e
-  show (NotYet n p) = "Nontermination: p_" ++ show n ++ "(...) = " ++
-                      show (stepsConfidence n)
+  show (NotYet n t _) = "Nontermination: p_" ++ show n ++ "(...) = " ++
+                      show (stepsConfidence n t)
 
 calude :: Term -> Integer -> Calude
 calude t m = case runThrowsPartial $ evaluation of
   Left (EvalFailure err) -> Failed err
   Right (Pure val) -> Normalized val
-  Right (Free w) -> NotYet m (wrap w)
+  Right (Free w) -> NotYet m t (wrap w)
   where evaluation = unwrapN m (eval [] t)
 
 flushStr :: String -> IO ()
@@ -39,7 +43,7 @@ readPrompt prompt = flushStr prompt >> getLine
 
 command :: Command -> Calude
 command (StepTest m t) = calude t m
-command (ConfidenceTest q t) = calude t (confidenceSteps q)
+command (ConfidenceTest q t) = calude t (confidenceSteps q t)
 
 readCommand :: String -> ThrowsError StuckState Command
 readCommand = lambdaParser . lexer
--- src/UntypedLambda.hs
+++ src/UntypedLambda.hs
@@ -3,15 +3,43 @@ module UntypedLambda where
 import Control.Monad.Except
 import Control.Monad.Identity
 import Control.Monad.Trans.Free
+import Foreign.Storable
 import PartialT
 import Debug.Trace
 
 data Command = StepTest Integer Term | ConfidenceTest Double Term deriving Show
 
+imLog :: Integer->Integer->Integer
+imLog b x =
+  if x < b then
+    0
+  else
+    let
+      l = 2 * imLog (b*b) x
+      doDiv x l = if x < b then l else doDiv (x`div`b) (l+1)
+    in
+      doDiv (x`div`(b^l)) l
+
+class ProgramSize a where
+  size :: a -> Integer
+instance ProgramSize Char where
+  size c = fromIntegral $ sizeOf c
+instance (ProgramSize a, ProgramSize b) => ProgramSize (a, b) where
+  size (x, y) = size x + size y
+instance ProgramSize a => ProgramSize (Maybe a) where
+  size (Just a) = 1 + size a
+  size Nothing  = 0
+instance ProgramSize a => ProgramSize [a] where
+  size as = (fromIntegral $ sizeOf $ length as) + (sum $ map size as)
+
 data Value = IntConst Integer | Abstraction String Term (Maybe Env)
 instance Show Value where
   show (IntConst n) = show n
   show (Abstraction str t _) = "λ" ++ str ++ "." ++ show t
+instance ProgramSize Value where
+  size (IntConst i) = 1 + imLog 2 i
+  size (Abstraction v t e) = 1 + size v + size t + size e
+
 data Term = Variable String | ValTerm Value | Application Term Term |
             Addition Term Term
 instance Show Term where
@@ -23,6 +51,11 @@ instance Show Term where
     _ -> "(" ++ show x ++ ")") ++ " + " ++ (case y of
     ValTerm (IntConst y') -> show y'
     _ -> "(" ++ show y ++ ")")
+instance ProgramSize Term where
+  size (Variable v)      = 2 + size v
+  size (ValTerm val)     = 2 + size val
+  size (Application f a) = 2 + size f + size a
+  size (Addition x y)    = 2 + size x + size y
 
 type Env = [(String, Value)]
 
